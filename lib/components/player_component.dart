aef8275c9b4b62452dc4bf1f1ce2cda7c074f67a
Trey Hope <trey.a.hope@gmail.com>
2022-11-20 03:30:43 -0500

    Created Joystick
    * Created joystick component.
    * User now mapped to joystick movement.

--- lib/components/player_component.dart
+++ lib/components/player_component.dart
@@ -1,23 +1,32 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:gift_grab/constants/globals.dart';
 import 'package:gift_grab/games/gift_grab_game.dart';
 
-class PlayerComponent extends SpriteComponent with HasGameRef<GiftGrabGame> {
-  final double spriteHeight = 200;
+class PlayerComponent extends SpriteComponent
+    with HasGameRef<GiftGrabGame>, CollisionCallbacks {
+  final double _spriteHeight = 200.0;
+  final double _maxSpeed = 300.0;
+  final JoystickComponent joystick;
+
+  PlayerComponent({required this.joystick});
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-
     sprite = await gameRef.loadSprite(Globals.playerSprite);
-
     position = gameRef.size / 2;
-    width = spriteHeight * 1.42;
-    height = spriteHeight;
+    width = _spriteHeight * 1.42;
+    height = _spriteHeight;
     anchor = Anchor.center;
   }
 
-  void move(Vector2 delta) {
-    position.add(delta);
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (joystick.direction != JoystickDirection.idle) {
+      position.add(joystick.relativeDelta * _maxSpeed * dt);
+      angle = joystick.delta.screenAngle();
+    }
   }
 }
--- lib/games/gift_grab_game.dart
+++ lib/games/gift_grab_game.dart
@@ -1,19 +1,18 @@
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
 import 'package:gift_grab/components/player_component.dart';
+import 'package:gift_grab/inputs/joystick.dart';
 
-class GiftGrabGame extends FlameGame with PanDetector {
-  final PlayerComponent _playerComponent = PlayerComponent();
+class GiftGrabGame extends FlameGame with HasDraggables {
+  final PlayerComponent _playerComponent = PlayerComponent(joystick: joystick);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
+    // Add player component.
     add(_playerComponent);
-  }
 
-  @override
-  void onPanUpdate(DragUpdateInfo info) {
-    _playerComponent.move(info.delta.game);
+    // Add joystick.
+    add(joystick);
   }
 }
