a595195f2b3f973babaa38863a79a8884177e7b8
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2023-08-20 20:43:25 -0500

    healthcheck RPC
    * Added successful RPC healthcheck function.

---
+++ healthcheck.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+type HealthcheckResponse struct {
+	Success bool `json: "success"`
+}
+
+func RpcHealthcheck(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error){
+	logger.Debug("Healthcheck RPC called")
+	response := &HealthcheckResponse{ Success: true }
+
+	out, err := json.Marshal(response)
+	if err != nil {
+		logger.Error("Error marshalling response type to JSON: %v", err)
+		return "", runtime.NewError("Cannon marshal type", 13)
+	}
+
+	return string(out), nil
+}
