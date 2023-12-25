fe11861ec101a0cd22e87d3188f3a0b888f2c2f6
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2023-12-25 13:16:16 -0500

    Update
    Updated

--- api/main.go
+++ api/main.go
@@ -1,20 +1,28 @@
 package main
 
-// func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
-// 	initStart := time.Now()
+import (
+	"context"
+	"database/sql"
+	"time"
 
-// 	id := "weekly_leaderboard"
-// 	authoritative := false
-// 	sortOrder := "desc"
-// 	operator := "best"
-// 	resetSchedule := "0 0 * * 1"
-// 	metadata := make(map[string]interface{})
+	"github.com/heroiclabs/nakama-common/runtime"
+)
 
-// 	if err := nk.LeaderboardCreate(ctx, id, authoritative, sortOrder, operator, resetSchedule, metadata); err != nil {
-// 		logger.Error("error creating leaderboard")
-// 		return err
-// 	}
+func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
+	initStart := time.Now()
 
-// 	logger.Info("Module loaded in %dms", time.Since(initStart).Milliseconds())
-// 	return nil
-// }
+	id := "weekly_leaderboard"
+	authoritative := false
+	sortOrder := "desc"
+	operator := "best"
+	resetSchedule := "0 0 * * 1"
+	metadata := make(map[string]interface{})
+
+	if err := nk.LeaderboardCreate(ctx, id, authoritative, sortOrder, operator, resetSchedule, metadata); err != nil {
+		logger.Error("error creating leaderboard")
+		return err
+	}
+
+	logger.Info("Module loaded in %dms", time.Since(initStart).Milliseconds())
+	return nil
+}
