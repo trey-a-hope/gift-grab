486f3b36126179aeec08cfbcbf299b0ca1d6ac29
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-12-23 18:44:59 -0500

    wip
    wip

--- lib/data/services/nakama_service.dart
+++ lib/data/services/nakama_service.dart
@@ -1,8 +1,15 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class NakamaService {
+  final _dio = Dio();
+
+  void authenticateEmail() {
+    _dio.post('', data: {});
+  }
+
   Future<void> auth({required String username}) async {
     try {
       const proxy =
