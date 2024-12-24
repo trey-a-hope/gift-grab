50d9e900b3fcb160477519fa7562e97720b4d1e4
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-12-24 10:22:06 -0500

    wip
    wip

--- lib/data/services/nakama_service.dart
+++ lib/data/services/nakama_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:gift_grab/domain/models/authenticate_email_response.dart';
+import 'package:gift_grab/domain/models/client_config.dart';
 
 class NakamaService {
   final _dio = Dio();
@@ -11,6 +12,14 @@ class NakamaService {
 
   String get _baseUrl => _isDevMode ? _baseUrlDev : _baseUrlProd;
 
+  final _clientConfig = ClientConfig(
+    host: '24.144.85.68',
+    ssl: false,
+    serverKey: 'defaultkey',
+    grpcPort: 7351,
+    httpPort: 7350,
+  );
+
   Future<AuthenticateEmailResponse> authenticateEmail({
     required String email,
     required String password,
@@ -21,10 +30,13 @@ class NakamaService {
       final res = await _dio.post(
         '$_baseUrl/authenticateEmail',
         data: {
-          'email': email,
-          'password': password,
-          'username': username,
-          'create': create,
+          "client": _clientConfig.toJson(),
+          "request": {
+            "email": email,
+            "password": password,
+            "username": username,
+            "create": true
+          }
         },
       );
 
