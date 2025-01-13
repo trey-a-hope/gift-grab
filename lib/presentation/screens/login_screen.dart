7a8cb147c30f3af5eb3e7f78c0b86a1fc28c436e
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-01-13 02:18:38 -0500

    wip
    wip

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -57,13 +58,15 @@ class LoginScreen extends StatelessWidget {
                 context: context,
               ),
             ),
-            LoginProvider(
-              icon: FontAwesomeIcons.apple,
-              label: 'Apple',
-              callback: () async => await _onLoginApple(
-                context: context,
+            if (Platform.isIOS) ...[
+              LoginProvider(
+                icon: FontAwesomeIcons.apple,
+                label: 'Apple',
+                callback: () async => await _onLoginApple(
+                  context: context,
+                ),
               ),
-            ),
+            ]
           ],
         ),
       );
