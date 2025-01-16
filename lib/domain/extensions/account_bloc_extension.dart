f30c0370149f1be71cdc341dc545cd3cb257f2a8
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-01-15 19:32:22 -0500

    AccountBlocX Deleted.
    * Removed AccountBlocX.

--- lib/domain/extensions/account_bloc_extension.dart
+++
@@ -1,8 +0,0 @@
-import 'package:gift_grab/domain/blocs/account/account_bloc.dart';
-
-extension AccountBlocX on AccountBloc {
-  String get uid => switch (state) {
-        AccountLoaded(account: final account) => account!.user.id,
-        _ => throw Exception('Account not loaded'),
-      };
-}
