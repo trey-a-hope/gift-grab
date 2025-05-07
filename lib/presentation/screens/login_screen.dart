cd502f75b35d580241d8c289c148d03f6d6e3555
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2025-05-07 12:29:52 -0400

    Teal Color Change
    * Color change to teal.

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -24,7 +24,7 @@ class LoginScreen extends StatelessWidget {
           savedPassword: _savedPassword,
           title: 'Gift Grab',
           theme: LoginTheme(
-            primaryColor: Colors.orange,
+            primaryColor: Colors.teal,
             accentColor: Colors.white,
           ),
           onLogin: (data) async => await _onLoginEmail(
