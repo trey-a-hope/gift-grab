cd8a1afc250999a86ddfa82c695b44d03220bfba
Trey Hope <trey.a.hope@gmail.com>
2025-07-25 15:49:19 -0500

    Update dependencies in pubspec.lock and remove unused game time limit constant in globals.dart
    - Updated versions of flame, flame_audio, and flame_bloc packages in pubspec.lock to their latest releases.
    - Removed the obsolete gameTimeLimit constant from globals.dart to streamline the codebase.

--- lib/data/constants/globals.dart
+++ lib/data/constants/globals.dart
@@ -13,10 +13,8 @@ class Globals {
 
   // Limits
   static const paginationLimit = 20;
-  static const gameTimeLimit = 10;
 
   // Durations
-
   static const String emptyProfile =
       'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
 }
