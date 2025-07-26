ddba0a07879c11b394aaf0fbeae36fd11f32fd0f
Trey Hope <trey.a.hope@gmail.com>
2025-07-26 13:01:48 -0500

    Add gift asset path to Globals class for improved asset management

--- lib/data/constants/globals.dart
+++ lib/data/constants/globals.dart
@@ -14,7 +14,9 @@ class Globals {
   // Limits
   static const paginationLimit = 20;
 
-  // Durations
+  // Image Paths
   static const String emptyProfile =
       'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+
+  static const String giftAsset = 'assets/images/gift-sprite.png';
 }
